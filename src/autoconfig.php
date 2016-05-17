490f4fa33cce174975eba27f4bc2debf5886d213
Marcel Veldhuizen <marcel@hellfire.egelantier.subbot.net>
2016-05-17 16:12:01 +0200

    Add initial version of source files
    Generating mail client autoconfiguration for Mozilla Thunderbird
    as well as Microsoft Outlook. The Mozilla format is also used by
    a number of other mail clients.

---
+++ src/autoconfig.php
@@ -0,0 +1,420 @@
+<?php
+
+class Configuration {
+    private $items = array();
+    
+    public function add($id) {
+        $result = new DomainConfiguration();
+        $result->id = $id;
+        array_push($this->items, $result);
+        return $result;
+    }
+    
+    public function getDomainConfig($domain) {
+        foreach ($this->items as $domainConfig) {
+            if (in_array($domain, $domainConfig->domains)) {
+                return $domainConfig;
+            }
+        }
+        
+        throw new Exception('No configuration found for requested domain.');
+    }
+}
+
+class DomainConfiguration {
+    public $domains;
+    public $servers = array();
+    public $username;
+    
+    public function addServer($type, $hostname) {
+        $server = $this->createServer($type, $hostname);
+        $server->username = $this->username;
+        array_push($this->servers, $server);
+        return $server;
+    }
+    
+    private function createServer($type, $hostname) {
+        switch ($type) {
+            case 'imap':
+                return new Server($type, $hostname, 143, 993);
+            case 'pop3':
+                return new Server($type, $hostname, 110, 995);
+            case 'smtp':
+                return new Server($type, $hostname, 25, 465);
+            default:
+                throw new Exception("Unrecognized server type \"$type\"");
+        }
+    }
+}
+
+class Server {
+    public $type;
+    public $hostname;
+    public $username;
+    public $endpoints;
+    public $samePassword;
+    
+    public function __construct($type, $hostname, $defaultPort, $defaultSslPort) {
+        $this->type = $type;
+        $this->hostname = $hostname;
+        $this->defaultPort = $defaultPort;
+        $this->defaultSslPort = $defaultSslPort;
+        $this->endpoints = array();
+        $this->samePassword = true;
+    }
+    
+    public function withUsername($username) {
+        $this->username = $username;
+        return $this;
+    }
+    
+    public function withDifferentPassword() {
+        $this->samePassword = false;
+        return $this;
+    }
+    
+    public function withEndpoint($socketType, $port = null, $authentication = 'password-cleartext') {
+        if ($port === null) {
+            $port = $socketType === 'SSL' ? $this->defaultSslPort : $this->defaultPort;
+        }
+        
+        array_push($this->endpoints, (object)array(
+            'socketType' => $socketType,
+            'port' => $port,
+            'authentication' => $authentication));
+            
+        return $this;
+    }
+    
+    
+}
+
+interface UsernameResolver {
+    public function findUsername($request);
+}
+
+class AliasesFileUsernameResolver implements UsernameResolver {
+    private $fileName;
+    
+    function __construct($fileName = "/etc/mail/aliases") {
+        $this->fileName = $fileName;
+    }
+    
+    public function findUsername($request) {
+        static $cachedEmail = null;
+        static $cachedUsername = null;
+        
+        if ($request->email === $cachedEmail) {
+            return $cachedUsername;
+        }
+        
+        $fp = fopen($this->fileName, 'rb');
+        
+        if ($fp === false) {
+            throw new Exception("Unable to open aliases file \"$fileName\"");
+        }
+        
+        $username =  $this->findLocalPart($fp, $request->localpart);
+        if (strpos($username, "@") !== false || strpos($username, ",") !== false) {
+            $username = null;
+        }
+         
+        $cachedEmail = $request->email;
+        $cachedUsername = $username;
+        return $username;        
+    }
+    
+    protected function findLocalPart($fp, $localPart) {
+        while (($line = fgets($fp)) !== false) {
+            $matches = array();
+            if (!preg_match("/^\s*" . preg_quote($localPart) . "\s*:\s*(\S+)\s*$/", $line, $matches)) continue;
+            return $matches[1];
+        }        
+    }
+}
+
+abstract class RequestHandler {
+    public function handleRequest() {
+        $request = $this->parseRequest();
+        $this->expandRequest($request);
+        $config = $this->getDomainConfig($request);
+        $this->writeResponse($config, $request);
+    }
+    
+    protected abstract function parseRequest();
+    protected abstract function writeResponse($config, $request);
+    
+    protected function expandRequest($request) {
+        list($localpart, $domain) = explode('@', $request->email);
+        
+        if (!isset($request->localpart)) {
+            $request->localpart = $localpart;
+        }
+
+        if (!isset($request->domain)) {
+            $request->domain = strtolower($domain);
+        }
+    }
+    
+    protected function getDomainConfig($request) {
+        static $cachedEmail = null;
+        static $cachedConfig = null;
+        
+        if ($cachedEmail === $request->email) {
+            return $cachedConfig;
+        }
+        
+        $cachedConfig = $this->readConfig($request);
+        $cachedEmail = $request->email;
+        
+        return $cachedConfig->getDomainConfig($request->domain);
+    }
+    
+    protected function readConfig($vars) {
+        foreach ($vars as $var => $value) {
+            $$var = $value;
+        }
+        
+        $config = new Configuration();
+        include './autoconfig.settings.php';
+        return $config;
+    }
+    
+    protected function getUsername($server, $request) {
+        if (is_string($server->username)) {
+            return $server->username;
+        }
+        
+        if ($server->username instanceof UsernameResolver) {
+            $resolver = $server->username;
+            return $resolver->findUsername($request);
+        }
+    }
+}
+
+class MozillaHandler extends RequestHandler {
+    public function writeResponse($config, $request) {
+        header("Content-Type: text/xml");
+        $writer = new XMLWriter();
+        $writer->openURI("php://output");
+    
+        $this->writeXml($writer, $config, $request);
+        $writer->flush();
+    }
+    
+    protected function parseRequest() {
+        return (object)array('email' => $_GET['emailaddress']);
+    }
+    
+    protected function writeXml($writer, $config, $request) {
+        $writer->startDocument("1.0");
+        $writer->setIndent(4);
+        $writer->startElement("clientConfig");
+        $writer->writeAttribute("version", "1.1");
+        
+        $this->writeEmailProvider($writer, $config, $request);
+        
+        $writer->endElement();
+        $writer->endDocument();
+    }   
+    
+    protected function writeEmailProvider($writer, $config, $request) {
+        $writer->startElement("emailProvider");
+        $writer->writeAttribute("id", $config->id);
+        
+        foreach ($config->domains as $domain) {
+            $writer->writeElement("domain", $domain);
+        }
+        
+        $writer->writeElement("displayName", $config->name);
+        $writer->writeElement("displayShortName", $config->nameShort);
+        
+        foreach ($config->servers as $server) {
+            foreach ($server->endpoints as $endpoint) {
+                $this->writeServer($writer, $server, $endpoint, $request);
+            }
+        }
+        
+        $writer->endElement();
+    }
+    
+    protected function writeServer($writer, $server, $endpoint, $request) {
+        switch ($server->type) {
+            case 'imap':
+            case 'pop3':
+                $this->writeIncomingServer($writer, $server, $endpoint, $request);
+                break;
+            case 'smtp':
+                $this->writeSmtpServer($writer, $server, $endpoint, $request);
+                break;
+        }
+    }
+    
+    protected function writeIncomingServer($writer, $server, $endpoint, $request) {
+        $authentication = $this->mapAuthenticationType($endpoint->authentication);
+        if (empty($authentication)) return;
+        
+        $writer->startElement("incomingServer");
+        $writer->writeAttribute("type", $server->type);
+        $writer->writeElement("hostname", $server->hostname);
+        $writer->writeElement("port", $endpoint->port);
+        $writer->writeElement("socketType", $endpoint->socketType);
+        $writer->writeElement("username", $this->getUsername($server, $request));
+        $writer->writeElement("authentication", $authentication);
+        $writer->endElement();
+    }
+    
+    protected function writeSmtpServer($writer, $server, $endpoint, $request) {
+        $authentication = $this->mapAuthenticationType($endpoint->authentication);
+        if ($authentication === null) return;
+                
+        $writer->startElement("outgoingServer");
+        $writer->writeAttribute("type", "smtp");
+        $writer->writeElement("hostname", $server->hostname);
+        $writer->writeElement("port", $endpoint->port);
+        $writer->writeElement("socketType", $endpoint->socketType);
+        
+        if ($authentication !== false) {
+            $writer->writeElement("username", $this->getUsername($server, $request));
+            $writer->writeElement("authentication", $authentication);
+        }
+        
+        $writer->writeElement("addThisServer", "true");
+        $writer->writeElement("useGlobalPreferredServer", "true");
+        $writer->endElement();
+    }
+    
+    protected function mapAuthenticationType($authentication) {
+        switch ($authentication) {
+            case 'password-cleartext':
+                return 'password-cleartext';
+            case 'CRAM-MD5':
+                return 'password-encrypted';
+            case 'none':
+                return false;
+            default:
+                return null;
+        }        
+    }
+}
+
+class OutlookHandler extends RequestHandler {
+    public function writeResponse($config, $request) {
+        header("Content-Type: application/xml");
+    
+        $writer = new XMLWriter();
+        $writer->openMemory();
+                
+        $this->writeXml($writer, $config, $request);
+        
+        $response = $writer->outputMemory(true);
+        echo $response;
+    }
+    
+    protected function parseRequest() {
+        $postdata = file_get_contents("php://input");
+        
+        if (strlen($postdata) > 0) {
+            $xml = simplexml_load_string($postdata);
+            return (object)array('email' => $xml->Request->EMailAddress);
+        }
+        
+        return null;
+    }
+    
+    public function writeXml($writer, $config, $request) {
+        $writer->startDocument("1.0", "utf-8");
+        $writer->setIndent(4);
+        $writer->startElement("Autodiscover");
+        $writer->writeAttribute("xmlns", "http://schemas.microsoft.com/exchange/autodiscover/responseschema/2006");
+        $writer->startElement("Response");
+        $writer->writeAttribute("xmlns", "http://schemas.microsoft.com/exchange/autodiscover/outlook/responseschema/2006a");
+        
+        $writer->startElement("Account");
+        $writer->writeElement("AccountType", "email");
+        $writer->writeElement("Action", "settings");
+        
+        foreach ($config->servers as $server) {
+            foreach ($server->endpoints as $endpoint) {
+                if ($this->writeProtocol($writer, $server, $endpoint, $request))
+                    break;
+            }
+        }        
+        
+        $writer->endElement();
+        
+        $writer->endElement();
+        $writer->endElement();
+        $writer->endDocument();
+    }
+    
+    protected function writeProtocol($writer, $server, $endpoint, $request) {
+        switch ($endpoint->authentication) {
+            case 'password-cleartext':
+            case 'SPA':
+                break;
+            case 'none':
+                if ($server->type !== 'smtp') return false;
+                break;
+            default:
+                return false;
+        }
+        
+        $writer->startElement('Protocol');
+        $writer->writeElement('Type', strtoupper($server->type));
+        $writer->writeElement('Server', $server->hostname);
+        $writer->writeElement('Port', $endpoint->port);
+        $writer->writeElement('LoginName', $this->getUsername($server, $request));
+        $writer->writeElement('DomainRequired', 'off');
+        $writer->writeElement('SPA', $endpoint->authentication === 'SPA' ? 'on' : 'off');
+        
+        switch ($endpoint->socketType) {
+            case 'plain':
+                $writer->writeElement("SSL", "off");
+                break;
+            case 'SSL':
+                $writer->writeElement("SSL", "on");
+                $writer->writeElement("Encryption", "SSL");
+                break;
+            case 'STARTTLS':
+                $writer->writeElement("SSL", "on");
+                $writer->writeElement("Encryption", "TLS");
+                break;
+        }
+        
+        $writer->writeElement("AuthRequired", $endpoint->authentication !== 'none' ? 'on' : 'off');
+        
+        if ($server->type == 'smtp') {
+            $writer->writeElement('UsePOPAuth', $server->samePassword ? 'on' : 'off');
+            $writer->writeElement('SMTPLast', 'off');
+        }
+        
+        $writer->endElement();
+        
+        return true;
+    }
+    
+    protected function mapAuthenticationType($authentication) {
+        switch ($authentication) {
+            case 'password-cleartext':
+                return 'password-cleartext';
+            case 'CRAM-MD5':
+                return 'password-encrypted';
+            case 'none':
+                return false;
+            default:
+                return null;
+        }        
+    }    
+    
+}
+
+if (strpos($_SERVER['SERVER_NAME'], "autoconfig.") === 0) {
+    // Configuration for Mozilla Thunderbird, Evolution, KMail, Kontact
+    $handler = new MozillaHandler();
+} else if (strpos($_SERVER['SERVER_NAME'], "autodiscover.") === 0) {
+    // Configuration for Outlook
+    $handler = new OutlookHandler();
+}
+
+$handler->handleRequest();
---
+++ src/autoconfig.settings.sample.php
@@ -0,0 +1,61 @@
+<?php
+// This configuration file is really just a plain PHP source file.
+// If needed, custom logic can be created.
+//
+// The following variables are available throughout the file:
+// $email       Full email address the settings are requested for
+// $domain      The domain part of the email address 
+// $localpart   The part before the @ sign of the email address
+
+// Add a new configuration set for a new group of domains. The argument
+// is an ID used in the Thunderbird autoconfiguration and can be anything.
+$cfg = $config->add('example.com');
+
+// Name and short name for the offered service, as used by Mozilla Thunderbird
+$cfg->name = 'Example mail services';
+$cfg->nameShort = 'Example';
+
+// Domains for which these settings apply, in lowercase.
+$cfg->domains = [ 'example.com', 'example.net', 'example.org' ];
+
+// This is the username associated with the email address. If some kind of lookup
+// needs to occur to map the email address to a username, this can be done by
+// implementing the UsernameResolver interface, or by just embedding the code here.
+// One such UsernameResolvers is currently built in, to aliases files used on many Linux systems.
+//
+// Some examples:
+//   "$localpart";                                          Use the localpart of the email address as username
+//   new AliasesFileUsernameResolver();                     Scan /etc/mail/aliases to obtain the username
+//   new AliasesFileUsernameResolver("/etc/mail/$domain");  Same but with separate file per domain
+$cfg->username = "$localpart";
+
+// Add available servers here.
+// addServer($type, $hostname)
+// $type             Server type. Possible types are: imap, pop3, smtp
+// $hostname         The host name or IP address of the server
+//
+// Each server should have one or more endpoints, defined by chaining one or more calls to withEndpoint:
+// withEndpoint($socketType, $port, $authenticatonType)
+// $socketType       Required and can be one of: plain, STARTTLS, SSL
+// $port             The port number on which the server will listen. Omit to use defaults.
+// $authentication   The authentication scheme to use:
+//                   password-cleartext     Default. Should be used only with STARTTLS or SSL
+//                   CRAM-MD5               Not supported by Outlook  
+//                   SPA                    Not supported by Thunderbird
+//                   none                   Only valid for SMTP. Not recommended however.
+
+// Example IMAP server for incoming mail, running on port 143 (TLS) and 993 (SSL)
+$cfg->addServer('imap', 'imap.example.com')
+    ->withEndpoint('STARTTLS')
+    ->withEndpoint('SSL');
+    
+// Example POP3 server for incoming mail, running on port 110 (TLS) and 995 (SSL)    
+$cfg->addServer('pop3', 'pop.example.com')
+    ->withEndpoint('STARTTLS')
+    ->withEndpoint('SSL');    
+
+// Example SMTP server for outgoing mail, running on port 587 (TLS) and 465 (SSL)
+$cfg->addServer('smtp', 'smtp.example.com')
+    ->withEndpoint('STARTTLS', 587)
+    ->withEndpoint('SSL');
+   
